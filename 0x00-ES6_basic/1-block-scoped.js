6617116c1dfcc384e0a27bae0dbc760d5f1963f5
Bassel-saleh <basselh26@gmail.com>
2024-09-30 19:39:51 +0300

    sm fixes #1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,13 +2,10 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  let newTask = task; // Create block-scoped variables to avoid reassignment
-  let newTask2 = task2;
-
   if (trueOrFalse) {
-    newTask = true; // Assign values to block-scoped variables
-    newTask2 = false;
+    const Task = true;
+    const Task2 = false;
   }
 
-  return [newTask, newTask2]; // Return the block-scoped variables
+  return [task, task2];
 }
