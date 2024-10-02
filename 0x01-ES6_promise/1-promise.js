4ee99b349b6b45adf0af72ea3fed0f02c379902b
Bassel-saleh <basselh26@gmail.com>
2024-10-02 04:57:04 +0300

    added task#1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reeject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reeject(new Error("The fake API is not working currently"));
+    }
+  });
+}
