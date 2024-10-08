1c8d2bff73c072188e5ffa95c800b4ac33bc3e1c
Bassel-saleh <basselh26@gmail.com>
2024-10-08 12:11:43 +0300

    added task#4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,16 @@
+export default function updateStudentGradeByCity(list, city, newGrades) {
+  const defaultGrade = { grade: 'N/A' };
+  if (list instanceof Array) {
+    return list
+      .filter((list) => list.location === city)
+      .map((list) => ({
+        id: list.id,
+        firstName: list.firstName,
+        location: list.location,
+        grade: (newGrades
+          .filter((grade) => grade.studentId === list.id)
+          .pop() || defaultGrade).grade,
+      }));
+  }
+  return {};
+}
