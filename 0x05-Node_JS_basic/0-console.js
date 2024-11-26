2947c7b2c82cc7b898fbaf0bbb0a820649213bd5
Bassel-saleh <basselh26@gmail.com>
2024-11-26 14:15:41 +0200

    added task#0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * prints a message on the STDOUT
+ * @param {String} msg The message to be printed
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
